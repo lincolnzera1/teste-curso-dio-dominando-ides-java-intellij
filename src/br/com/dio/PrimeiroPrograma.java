4a757d90ed060a5f4b50e9b8c462b4a111e7e0a5
Guilherme Lincoln <guilherme.lincoln.lima.11@gmail.com>
2021-08-12 16:26:23 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+
+        System.out.println(cachorro);
+
+
+
+    /*public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Olá, Mundo! " + (a+b));
+    }*/
+    }
+}
